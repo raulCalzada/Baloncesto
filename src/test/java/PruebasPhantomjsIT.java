1aa6d5cb8b21a22eeb9021bc0c75d40bdaedbd92
Raul Calzada Maldonado <raul.calzada.maldonado@gmail.com>
2024-02-22 12:58:51 +0100

    Veamos error en prueba funcional

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -13,13 +13,16 @@ public class PruebasPhantomjsIT {
     public void tituloIndexTest() {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setJavascriptEnabled(true);
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/bin/phantomjs");
+
+
         caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {"--web-security=no", "--ignore-ssl-errors=yes"});
-        
+      
+
         driver = new PhantomJSDriver(caps);
         driver.navigate().to("http://localhost:8080/Baloncesto/");
         
         assertEquals("Votacion mejor jugador liga ACB", driver.getTitle(), "El titulo no es correcto");
+
         System.out.println(driver.getTitle());
         
         driver.close();
