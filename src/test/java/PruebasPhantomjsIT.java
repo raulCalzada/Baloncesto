ef07d3d567bb3cc25e1aa8682d360822f650f0ef
Raul Calzada Maldonado <raul.calzada.maldonado@gmail.com>
2024-02-24 00:29:57 +0100

    prueba intento 1

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -1,10 +1,13 @@
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
 
 public class PruebasPhantomjsIT {
     private static WebDriver driver = null;
@@ -28,5 +31,27 @@ public class PruebasPhantomjsIT {
         
         driver.close();
         driver.quit();
+    }
+
+    @Test
+    public void ponerVotosACeroYVerificar() {
+        // Abre la página principal
+        driver.navigate().to("http://localhost:8080/Baloncesto/");
+
+        // Pulsar el botón "Poner votos a cero"
+        driver.findElement(By.name("B2")).click();
+
+        // Pulsar el botón "Ver votos"
+        driver.findElement(By.xpath("//button[contains(text(),'Tabla Votos')]")).click();
+
+        // Comprobar que los votos para cada jugador son cero en la página "VerVotos.jsp"
+        driver.navigate().to("http://localhost:8080/Baloncesto/VerVotos.jsp");
+
+        // Comprobar que los votos para cada jugador son cero en la página "VerVotos.jsp"
+        List<WebElement> elementosVotos = driver.findElements(By.xpath("//table//tr//td[2]"));
+        for (WebElement elemento : elementosVotos) {
+            assertEquals("0", elemento.getText(), "El voto no es cero");
+        }
+
     }
 }
