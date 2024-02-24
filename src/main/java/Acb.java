b92a9f75f3e0b09b62b597b261fe0072108d7cc3
Raul Calzada Maldonado <raul.calzada.maldonado@gmail.com>
2024-02-24 02:31:56 +0100

    ahora si arreglo punto y coma

--- src/main/java/Acb.java
+++ src/main/java/Acb.java
@@ -20,7 +20,8 @@ public class Acb extends HttpServlet {
         String nombre = (String) req.getParameter("R1");
         if (nombre == null) {
             try {bd.resetearVotos();} catch (Exception e) { // Ignorar excepción porque no es crítica en este contexto
-                // No necesitamos tomar medidas adicionales ya que no afecta al flujo principal};
+                // No necesitamos tomar medidas adicionales ya que no afecta al flujo principal
+            };
             return;
         };
         if (nombre.equals("Otros")) {
