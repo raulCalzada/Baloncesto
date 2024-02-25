39c8be107e8b647c2d9376f25ccba9cab1cf0cd1
Raul Calzada Maldonado <raul.calzada.maldonado@gmail.com>
2024-02-25 12:31:05 +0100

    arreglo pf-A

--- src/main/java/Acb.java
+++ src/main/java/Acb.java
@@ -16,26 +16,34 @@ public class Acb extends HttpServlet {
     @Override
     public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         HttpSession s = req.getSession(true);
-        String nombreP = (String) req.getParameter("txtNombre");
-        String nombre = (String) req.getParameter("R1");
-        if (nombre == null) {
-            try {bd.resetearVotos();} catch (Exception e) { // Ignorar excepción porque no es crítica en este contexto
-                // No necesitamos tomar medidas adicionales ya que no afecta al flujo principal
-            };
+        String nombreP = req.getParameter("txtNombre");
+        String nombre = req.getParameter("R1");
+        String reset = req.getParameter("R2");
+
+        if ("true".equals(reset)) {
+            try {
+                bd.resetearVotos();
+            } catch (Exception e) {
+                // Manejar la excepción si es necesario
+                e.printStackTrace();
+            }
             return;
-        };
-        if (nombre.equals("Otros")) {
-            nombre = (String) req.getParameter("txtOtros");
         }
-        if (bd.existeJugador(nombre)) {
-            bd.actualizarJugador(nombre);
-        } else {
-            bd.insertarJugador(nombre);
-        }
-        s.setAttribute("nombreCliente", nombreP);
-        // Llamada a la página jsp que nos da las gracias
-        res.sendRedirect(res.encodeRedirectURL("TablaVotos.jsp"));
+
+        if (nombre != null) {
+            if (nombre.equals("Otros")) {
+                nombre = req.getParameter("txtOtros");
+            }
+            if (bd.existeJugador(nombre)) {
+                bd.actualizarJugador(nombre);
+            } else {
+                bd.insertarJugador(nombre);
+            }
     }
+    
+    s.setAttribute("nombreCliente", nombreP);
+    res.sendRedirect(res.encodeRedirectURL("TablaVotos.jsp"));
+}
 
     @Override
     public void destroy() {
